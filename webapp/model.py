da219c697cbdc982a97741c4b1dc0e7aa53afff8
Dmitriy Stanevich <stan_1@list.ru>
2019-01-24 00:25:10 +0300

    скорректировал модель базы

--- webapp/model.py
+++ webapp/model.py
@@ -6,19 +6,18 @@ db = SQLAlchemy()
 class Notice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notice_id = db.Column(db.Integer, nullable=False)
-	vendor_definition_method = db.Column(db.Integer, nullable=False)
-	vendor = db.Column(db.String, nullable=False)
+	vendee_definition_method = db.Column(db.Integer, nullable=False)
 	tender_system = db.Column(db.String, nullable=False)
 	start_max_price = db.Column(db.String, nullable=False)
     application_dates_end = db.Column(db.DateTime, nullable=False)
     e_auction_date = db.Column(db.DateTime, nullable=False)
     date_printed_notice = db.Column(db.DateTime, nullable=False)
 
-class Vendor(db.Model):
+class Vendee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notice_id = db.Column(db.Integer, nullable=False)
 	postal_address = db.Column(db.Integer, nullable=False)
-	vendor = db.Column(db.String, nullable=False)
+	vendee = db.Column(db.String, nullable=False)
 
 class Offer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,8 +33,8 @@ class Offer(db.Model):
 			
 #Извещение:			
 #Номер извещения - notice_id
-#Способ определения поставщика (подрядчика, исполнителя) - vendor_definition_method
-#Организация, осуществляющая размещение - vendor
+#Способ определения поставщика (подрядчика, исполнителя) - vendee_definition_method
+#Организация, осуществляющая размещение - vendee
 #Адрес электронной площадки в информационно-телекоммуникационной сети «Интернет» - tender_system
 #Начальная (максимальная) цена контракта - start_max_price
 #Дата и время окончания подачи заявок - application_dates_end
@@ -45,7 +44,7 @@ class Offer(db.Model):
 #Организация:
 #Номер извещения - notice_id
 #Почтовый адрес - postal_address
-#Организация, осуществляющая размещение - vendor
+#Организация, осуществляющая размещение - vendee
 
 #Товар:
 #Номер извещения - notice_id
