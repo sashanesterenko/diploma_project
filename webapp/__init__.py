e713b9b8b3961eba3b65973f5d95fd34b2042dd7
Dmitriy Stanevich <stan_1@list.ru>
2019-02-02 09:42:09 +0300

    Ошибка с созданием базы данных

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,10 +4,13 @@ from webapp.model import db
 
 def create_app():
     app = Flask(__name__)
+
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
     @app.route('/')
     def index():
         title = "Парсер > БД"
+        return 'Привет'
+
     return app
