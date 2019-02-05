ea8c1b7c4b430f995a046560f1e38a0f58407634
Sasha Nesterenko <nesterenko.sasha@gmail.com>
2019-02-05 22:16:32 +0200

    Updated webapp

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 from webapp.model import db
+from webapp.forms import SubmitForm
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    @app.route('/')
-    def index():
-        title = "Парсер > БД"
-        return 'Привет'
+    @app.route('/submit_links', methods=['GET','POST'])
+    def links_submission():
+        title = "Добавление ссылок в БД"
+        submit_a_link_form = SubmitForm()
+        if submit_a_link_form.validate_on_submit():
+            if request.form.post == submit_link: 
+                print(submit_a_link_form.a_link.data)
+	       		
+        return render_template('links_submission.html', page_title=title, form=submit_a_link_form)
+
+
 
     return app
---
+++ webapp/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class SubmitForm(FlaskForm):
+    a_link = StringField('Ссылка на печатную форму извещения', validators=[DataRequired()])
+    submit_link = SubmitField('Отправить ссылки')
+    submit_another_link = SubmitField('Добавить ещё одну ссылку')
